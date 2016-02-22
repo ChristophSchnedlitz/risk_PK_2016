14364dffd166d7c621548cb9fb191526893241e6
ChristophSchnedlitz <christoph.schnedlitz@gmail.com>
2016-02-22 17:24:19 +0100

    Added files via upload

---
+++ Risk/src/Main.java
@@ -0,0 +1,26 @@
+import Import.ReadFile;
+import MapClasses.Territory;
+
+/**
+ * Created by Christoph on 22.02.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        String filepath = "C:\\Users\\Christoph\\Documents\\world.map";
+
+        ReadFile importer = new ReadFile();
+        importer.readFile(filepath);
+
+        System.out.println(Territory.tmap.get("Japan").patches);
+
+
+    }
+
+
+
+
+
+
+}
